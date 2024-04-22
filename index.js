70da2c2230d5416c615bc5c6b7fa0d72012dfc26
annmuros <amurosdel@gmail.com>
2024-04-22 19:55:45 +0200

    updates
    select DOM elements and assign variables

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-//seleciono lops elementos del DOM y asigno variables
+//seleciono los elementos del DOM y asigno variables
 
 let totalAmount = document.getElementById("total-amount");// aqui entro la cantidad total de presupuesto
+let userAmount = document.getElementById("user-amount"); // aqui entrada de lo que cuesta el producto: alquiler, comida, gas, luz...
+const checkAmountButton = document.getElementById("check-amount");//boton que añade los gastos de las expenses
+
+const totalAmountButton = document.getElementById("total-amount-button");//boton que añade el presupuesto total que le indicamos
+
+const productTitle = document.getElementById("product title");//input del nombre del producto que añadimos
+
+const productTitleError = document.getElementById("product-title");//Mensaje de error para el título del producto
+
+const productCostError = document.getElementById("product-cost-error");//Mensaje de error para el costo del producto
+
+const amount = document.getElementById("amount");//muestra el total del presupuesto 
+const expenditureValue = document.getElementById("expenditure-value");//muestra el total de los gastos
+const balanceValue = document.getElementById("balance-amount"); // Elemento que muestra el saldo restante después de los gastos
+
+const list = document.getElementById("list"); // Elemento que representa la lista de gastos, dónde vamos añadiendo los gastos
+
+let tempAmount = 0; // Variable temporal para almacenar el valor del monto total ingresado por el usuario
+
+
+
+
