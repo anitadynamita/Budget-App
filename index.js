43a1ca000cc642ed8635aea13d65a26886843542
annmuros <amurosdel@gmail.com>
2024-04-22 20:44:35 +0200

    updates
    error messages are not working : (

--- index.js
+++ index.js
@@ -8,9 +8,15 @@ const totalAmountButton = document.getElementById("total-amount-button");//boton
 
 const productTitle = document.getElementById("product title");//input del nombre del producto que añadimos
 
-const productTitleError = document.getElementById("product-title");//Mensaje de error para el título del producto
+//AQUÍ MENSAJES DE ERRROR//
 
-const productCostError = document.getElementById("product-cost-error");//Mensaje de error para el costo del producto
+const errorMessage = document.getElementById("budget-error");//Almacena la referencia al elemento HTML que muestra un mensaje de error si el monto total ingresado es negativo o vacío.
+
+const productTitleError = document.getElementById("product-title");//Almacena la referencia al elemento HTML que muestra un mensaje de error si el título del producto está vacío.
+
+const productCostError = document.getElementById("product-cost-error");//Almacena la referencia al elemento HTML que muestra un mensaje de error si el costo del producto está vacío.
+
+//AQUÍ MENSAJES DE ERRROR//
 
 const amount = document.getElementById("amount");//muestra el total del presupuesto 
 const expenditureValue = document.getElementById("expenditure-value");//muestra el total de los gastos
@@ -22,4 +28,28 @@ let tempAmount = 0; // Variable temporal para almacenar el valor del monto total
 
 
 
+//1. EVENT LISTENER PARA INDICAR DEL PRESUPUESTO QUE DISPONEMOS
 
+totalAmountButton.addEventListener("click", () => {
+    // Guardar el valor del monto total ingresado por el usuario
+    tempAmount = totalAmount.value;
+
+
+
+   // Verificar si la cantidad está vacía o es negativa
+  if (tempAmount === "" || tempAmount <= 0) { //Comprueba si tempAmount es una cadena vacía o si es menor o igual a cero o 0.
+    errorMessage.classList.remove("hide"); // Mostrar mensaje de ERROR
+
+  } else {
+    errorMessage.classList.add("hide"); // Ocultar mensaje de ERROR
+    
+    // Actualizar el valor del presupuesto
+    amount.innerHTML = tempAmount;
+    
+    // Actualizar el valor del saldo
+    balanceValue.innerText = tempAmount - expenditureValue.innerText;
+    
+    // Limpiar el campo de entrada de la cantidad total
+    totalAmount.value = "";
+  }
+});
